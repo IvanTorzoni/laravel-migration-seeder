3f568e8ce35270f7dd0fc4898a36e02a27733e86
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-11 14:44:16 +0200

    feat: added table visualization of the trains

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -3,7 +3,36 @@
 @section('content')
     <h1>Pagina Treni</h1>
 
-    @foreach ($trains as $train)
-        <li> {{ $train->azienda }} {{ $train->stazione_partenza }} {{ $train->stazione_arrivo }} {{ $train->orario_partenza }} {{ $train->orario_arrivo }} {{ $train->codice_treno }} {{ $train->numero_carrozze }} {{ $train->in_orario }} {{ $train->in_ritardo }} </li>
-    @endforeach
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">N. Treno</th>
+                <th scope="col">Azienda</th>
+                <th scope="col">Stazione di partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Orario di partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">N. Carrozze</th>
+                <th scope="col">In Orario</th>
+                <th scope="col">In Ritardo</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <th scope="row">{{ $train->id }}</th>
+                    <td>{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_partenza }}</td>
+                    <td>{{ $train->stazione_arrivo }}</td>
+                    <td>{{ $train->orario_partenza }}</td>
+                    <td>{{ $train->orario_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->numero_carrozze }}</td>
+                    <td>{{ $train->in_orario === 1 ? 'si' : 'no'}}</td>
+                    <td>{{ $train->in_ritardo === 0 ? 'si' : 'no'}}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
