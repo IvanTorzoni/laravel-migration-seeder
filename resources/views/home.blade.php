729452060739a5611e056e8f1a5f59e634957ad0
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-11 15:35:09 +0200

    feat: added home visualization of array with dd instruction

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Home page</h1>
+
+    <h1>Tabella dei treni in partenza oggi</h1>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">N. Treno</th>
+                <th scope="col">Azienda</th>
+                <th scope="col">Stazione di partenza</th>
+                <th scope="col">Stazione di Arrivo</th>
+                <th scope="col">Data di partenza</th>
+                <th scope="col">Orario di partenza</th>
+                <th scope="col">Orario di Arrivo</th>
+                <th scope="col">Codice Treno</th>
+                <th scope="col">N. Carrozze</th>
+                <th scope="col">In Orario</th>
+                <th scope="col">In Ritardo</th>
+            </tr>
+        </thead>
+        {{-- <tbody>
+            @foreach ($trains as $train)
+                <tr>
+                    <th scope="row">{{ $train->id }}</th>
+                    <td>{{ $train->azienda }}</td>
+                    <td>{{ $train->stazione_partenza }}</td>
+                    <td>{{ $train->stazione_arrivo }}</td>
+                    <td>{{ $train->data_di_partenza }}</td>
+                    <td>{{ $train->orario_partenza }}</td>
+                    <td>{{ $train->orario_arrivo }}</td>
+                    <td>{{ $train->codice_treno }}</td>
+                    <td>{{ $train->numero_carrozze }}</td>
+                    <td>{{ $train->in_orario === 1 ? 'si' : 'no'}}</td>
+                    <td>{{ $train->in_ritardo === 0 ? 'si' : 'no'}}</td>
+                </tr>
+            @endforeach
+        </tbody> --}}
+    </table>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/trains', [TrainController::class, 'index'])->name('trains');
