32c37ca120bda950230056e52c09cfd276dd7228
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-06-11 14:22:42 +0200

    feat: migration of structure of the table

--- database/migrations/2024_06_11_110322_create_trains_table.php
+++ database/migrations/2024_06_11_110322_create_trains_table.php
@@ -8,11 +8,33 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     * Create TABLE ‘houses’ (
+     * ‘id’ Unsigned BIGINT NOT NULL AUTO_INCREMENT,
+     * ‘azienda’ VARCHAR(255) NOT NULL,
+     * ‘stazione_partenza’ VARCHAR(255) NOT NULL,
+     * ‘stazione_arrivo’ VARCHAR(255) NOT NULL,
+     * ‘orario_partenza’ TIME NOT NULL,
+     * ‘orario_arrivo’ TIME NOT NULL,
+     * ‘codice_treno’ VARCHAR(255) NOT NULL,
+     * ‘numero_carrozze’ UNSIGNET TINYINT NOT NULL,
+     * ‘in_orario’ TINIYNT NOT NULL DEFAULT(1)
+     * ‘in_ritardo’ TINIYNT NOT NULL DEFAULT(0)
+     * );
      */
+
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_partenza');
+            $table->string('stazione_arrivo');
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('codice_treno');
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(1);
+            $table->boolean('in_ritardo')->default(0);
             $table->timestamps();
         });
     }
